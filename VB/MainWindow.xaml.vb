d113220740aa34b678fcbb9fa224f873539dea74
DevExpressExampleBot <fakeEmail>
2023-11-16 13:34:42 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,33 +1,33 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.Gauges
 
 Namespace Digital_Gauge
 
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-'			#Region "#SetSymbolSize"
-			' Specify fixed values for symbol height and width.
-			Dim newWidth As New SymbolLength(SymbolLengthType.Fixed, 44)
-			matrixView.Width = newWidth
-			Dim newHeight As New SymbolLength(SymbolLengthType.Fixed, 50)
-			matrixView.Height = newHeight
-'			#End Region
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-'			#Region "#SetSymbolSize"
-			' Specify proportional values for symbol height and width.
-			Dim newWidth As New SymbolLength(SymbolLengthType.Proportional, 2)
-			matrixView.Width = newWidth
-			Dim newHeight As New SymbolLength(SymbolLengthType.Proportional, 5)
-			matrixView.Height = newHeight
-'			#End Region
-		End Sub
-	End Class
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+'#Region "#SetSymbolSize"
+            ' Specify fixed values for symbol height and width.
+            Dim newWidth As SymbolLength = New SymbolLength(SymbolLengthType.Fixed, 44)
+            Me.matrixView.Width = newWidth
+            Dim newHeight As SymbolLength = New SymbolLength(SymbolLengthType.Fixed, 50)
+            Me.matrixView.Height = newHeight
+'#End Region  ' #SetSymbolSize
+        End Sub
+
+        Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+'#Region "#SetSymbolSize"
+            ' Specify proportional values for symbol height and width.
+            Dim newWidth As SymbolLength = New SymbolLength(SymbolLengthType.Proportional, 2)
+            Me.matrixView.Width = newWidth
+            Dim newHeight As SymbolLength = New SymbolLength(SymbolLengthType.Proportional, 5)
+            Me.matrixView.Height = newHeight
+'#End Region  ' #SetSymbolSize
+        End Sub
+    End Class
 End Namespace
