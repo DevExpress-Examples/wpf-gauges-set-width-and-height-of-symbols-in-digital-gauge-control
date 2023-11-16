27d38f6341caa6a4a944cbd33a05478447255c24
AndreySlabov <andrey.slabov@devexpress.com>
2023-11-16 12:36:37 +0400

    Format Example

--- CS/MainWindow.xaml.cs
+++ CS/MainWindow.xaml.cs
@@ -9,23 +9,15 @@ namespace Digital_Gauge {
         }
 
         private void button1_Click(object sender, RoutedEventArgs e) {
-            #region #SetSymbolSize
             // Specify fixed values for symbol height and width.
-            SymbolLength newWidth = new SymbolLength(SymbolLengthType.Fixed, 44);
-            matrixView.Width = newWidth;
-            SymbolLength newHeight = new SymbolLength(SymbolLengthType.Fixed, 50);
-            matrixView.Height = newHeight;
-            #endregion #SetSymbolSize
+            matrixView.Width = new SymbolLength(SymbolLengthType.Fixed, 44);
+            matrixView.Height = new SymbolLength(SymbolLengthType.Fixed, 50);
         }
 
         private void button2_Click(object sender, RoutedEventArgs e) {
-            #region #SetSymbolSize
             // Specify proportional values for symbol height and width.
-            SymbolLength newWidth = new SymbolLength(SymbolLengthType.Proportional, 2);
-            matrixView.Width = newWidth;
-            SymbolLength newHeight = new SymbolLength(SymbolLengthType.Proportional, 5);
-            matrixView.Height = newHeight;
-            #endregion #SetSymbolSize
+            matrixView.Width = new SymbolLength(SymbolLengthType.Proportional, 2);
+            matrixView.Height = new SymbolLength(SymbolLengthType.Proportional, 5);
         }
     }
 }
