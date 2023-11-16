88be616654980b69b7b2876788dee1d84e02ee55
DevExpressExampleBot <fakeEmail>
2023-11-16 12:44:18 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -11,23 +11,15 @@ Namespace Digital_Gauge
         End Sub
 
         Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-'#Region "#SetSymbolSize"
             ' Specify fixed values for symbol height and width.
-            Dim newWidth As SymbolLength = New SymbolLength(SymbolLengthType.Fixed, 44)
-            Me.matrixView.Width = newWidth
-            Dim newHeight As SymbolLength = New SymbolLength(SymbolLengthType.Fixed, 50)
-            Me.matrixView.Height = newHeight
-'#End Region  ' #SetSymbolSize
+            Me.matrixView.Width = New SymbolLength(SymbolLengthType.Fixed, 44)
+            Me.matrixView.Height = New SymbolLength(SymbolLengthType.Fixed, 50)
         End Sub
 
         Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-'#Region "#SetSymbolSize"
             ' Specify proportional values for symbol height and width.
-            Dim newWidth As SymbolLength = New SymbolLength(SymbolLengthType.Proportional, 2)
-            Me.matrixView.Width = newWidth
-            Dim newHeight As SymbolLength = New SymbolLength(SymbolLengthType.Proportional, 5)
-            Me.matrixView.Height = newHeight
-'#End Region  ' #SetSymbolSize
+            Me.matrixView.Width = New SymbolLength(SymbolLengthType.Proportional, 2)
+            Me.matrixView.Height = New SymbolLength(SymbolLengthType.Proportional, 5)
         End Sub
     End Class
 End Namespace
